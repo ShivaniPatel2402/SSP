58d9405a7339af2a7fa544259e1bc9f83b87de8a
ShivaniPatel2402 <113051028+ShivaniPatel2402@users.noreply.github.com>
2022-11-03 18:43:24 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,13 @@
 import * as React from "react"
 import { Link } from "gatsby"
-import "../components/index.css"
-
+import GatsbyLink from "gatsby-link"
 const Index = () => {
-    return (
-    <div>
-      <h1>My Landing Page</h1>
-    <p>This is my landing page</p>
-    </div>
-    )
-   }
-   export default Index
+ return (
+ <div>
+        <Link to="about">About Page</Link>
+  <h1>My Landing Page</h1>
+  <p>This is my landing page</p>
+  </div>
+ )
+}
+export default Index
