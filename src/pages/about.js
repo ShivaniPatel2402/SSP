7692fc8adabc98b49d7f074f5bc2c70d088ec67d
ShivaniPatel2402 <113051028+ShivaniPatel2402@users.noreply.github.com>
2022-11-03 18:38:07 -0400

    First Edit (2)

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,13 @@
 import * as React from 'react'
-import { Link } from 'gatsby' 
+import { Link } from 'gatsby'
 const About = () => {
     return (
-    <div>
+        <div>
         <Link to="/">Home Page</Link>
-    <h1>My About Page</h1>
-    <p>This is my About page</p>
-    </div>
+            <h1>My About Page</h1>
+            <p>This is my About Page</p>
+        </div>
+
     )
-    }
-    export default About
+}
+export default About
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,13 @@
 import * as React from "react"
 import { Link } from "gatsby"
+import "../components/index.css"
+
 const Index = () => {
     return (
     <div>
-        <Link to="about">About Page</Link>
-    <h1>My Landing Page</h1>
+      <h1>My Landing Page</h1>
     <p>This is my landing page</p>
     </div>
     )
-   } 
-export default Index
+   }
+   export default Index
