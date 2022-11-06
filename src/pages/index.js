037c68050dfbf993aacc5d489ef9db17230997ff
ShivaniPatel2402 <113051028+ShivaniPatel2402@users.noreply.github.com>
2022-11-06 12:18:11 -0500

    go

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,22 @@
 import * as React from "react"
 import { Link } from "gatsby"
+import "..components/index.css"
+
+
+
 const Index = () => {
- return (
- <div>
+   return (
+      <header> className='hero'
+      <div> className='heroContainer'
+            <div> className='heroInfo'
+            <h1>Shivani S. Patel</h1>
+            <p>Cybersecurity and Information</p>
+         <Link to="/about">About Page</Link>
          <h1>My Landing Page</h1>
          <p>This is my landing page</p>
-   </div>
-   )
+         </div>
+      </div>
+      </header>
+   );
   }
   export default Index
