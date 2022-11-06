483f0f33530571be85ba7bb9c5831cdc1ee9c007
ShivaniPatel2402 <113051028+ShivaniPatel2402@users.noreply.github.com>
2022-11-06 11:57:27 -0500

    first edits 2

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,11 @@
 import * as React from "react"
 import { Link } from "gatsby"
-import GatsbyLink from "gatsby-link"
 const Index = () => {
  return (
  <div>
-        <Link to="about">About Page</Link>
-  <h1>My Landing Page</h1>
-  <p>This is my landing page</p>
-  </div>
- )
-}
-export default Index
+         <h1>My Landing Page</h1>
+         <p>This is my landing page</p>
+   </div>
+   )
+  }
+  export default Index
