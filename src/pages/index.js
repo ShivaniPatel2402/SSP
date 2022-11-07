fdd22896f993f3fff03a5abc1b2e5721ebe2e203
ShivaniPatel2402 <113051028+ShivaniPatel2402@users.noreply.github.com>
2022-11-06 19:31:41 -0500

    both updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,20 @@
-import * as React from "react"
-import { Link } from "gatsby"
-import "..components/index.css"
-
+import * as React from "react";
+import { Link } from "gatsby";
+import "../components/index.css"
 
 
 const Index = () => {
    return (
-      <header> className='hero'
-      <div> className='heroContainer'
-            <div> className='heroInfo'
-            <h1>Shivani S. Patel</h1>
-            <p>Cybersecurity and Information</p>
-         <Link to="/about">About Page</Link>
-         <h1>My Landing Page</h1>
-         <p>This is my landing page</p>
-         </div>
+   <header> className= 'hero'
+      <div>className='heroContainer'
+         <div>className='heroInfo'
+         <h1>Shivani S. Patel</h1>
+         <p>Cybersecurity and Information Design</p>
+         <button className='heroButton' onClick={()=>{navigate("about")}}>How We Can Help</button>
       </div>
-      </header>
+   </div>
+</header>
    );
-  }
+}
+   
   export default Index
